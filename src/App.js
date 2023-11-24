763185bc16c00afa74231f8200315ee994bc62d8
AlinaLevi <alina.levi.art@gmailcom>
2023-11-23 18:50:43 -0600

    Adds home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import bckgrnd from './images/1.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Comming soon...
-      </header>
+      <img className="background" src={bckgrnd} alt="Background"/>
+      <div className="description">Through my art, I strive to create a visual language that resonates with viewers, evoking a range of emotions and provoking contemplation.<br/> Each brushstroke, color choice, and hidden symbol serves to convey a narrative that goes beyond the surface, inviting viewers to explore the multifaceted nature of the human experience.</div>
     </div>
   );
 }
