a425e0e52d084af2234a7be3a7b6495afa57f81c
AlinaLevi <alina.levi.art@gmailcom>
2023-11-24 16:24:45 -0600

    Fix overflow on small sceens, adjust background animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.scss';
 import bckgrnd from './images/1.jpg'
+import DynamicBackground from './DynamicBackground';
 
 function App() {
   return (
     <div className="App">
+      <DynamicBackground/>
       <img className="background" src={bckgrnd} alt="Background"/>
       <div className="description">Through my art, I strive to create a visual language that resonates with viewers, evoking a range of emotions and provoking contemplation.<br/> Each brushstroke, color choice, and hidden symbol serves to convey a narrative that goes beyond the surface, inviting viewers to explore the multifaceted nature of the human experience.</div>
     </div>
---
+++ src/DynamicBackground.js
@@ -0,0 +1,15 @@
+
+import './DynamicBackground.scss';
+
+function DynamicBackground() {
+  return (
+    <div className="DynamicBackground-outer">
+        <div className="DynamicBackground-inner">
+            <div className="DynamicBackground-innermost">
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default DynamicBackground;
